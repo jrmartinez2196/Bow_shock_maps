2949d0dd78bd7f37804582da62b029ff20c59182
jrmartinez2196 <jrmartinez2196@gmail.com>
2026-05-10 17:56:03 -0300

    Deleted unused functions

--- bow_shock_surface.py
+++ bow_shock_surface.py
@@ -6,11 +6,48 @@ from scipy.interpolate import interp1d
 from constants import AU, Msun, year, mu, mp
 
 def standoff_distance(Mdot, Vw, Vstar, n_ism):
+    '''
+    Calculates the distance to the termination shock at te apex (Wilkin 1997)
+    Ignores ISM thermal pressure
+
+    Parameters: (all in cgs units)
+    -----------
+    Mdot : float
+        Wind mass-loss rate
+    Vw : float
+        Stellar wind velocity
+    Vstar : float
+        Stellar velocity
+    n_ism : float
+        ISM numerical density
+
+    Returns:
+    --------
+    R0 : float
+        Termination shock distance at the apex
+    '''
     rho_ism = n_ism * mu * mp
     R0 = np.sqrt(Mdot* Vw / (4 * np.pi * rho_ism * Vstar**2))
     return R0
 
 def _rhs_christie(theta, r, lam, tiny=1e-14):
+    '''
+    Right hand side of Eq. 11 (EDO) from Christie+ 2016
+
+    Parameters:
+    -----------
+    theta : float
+        Angle measured from the apex to the bow shock tail
+    r : float
+        Normalized distance to the bow shock
+    lam : float
+        lam = alpha/(1+alpha) (Eq. 8), alpha being the thermal pressure fraction
+
+    Returns:
+    --------
+    num/denom : float
+        r' = dr/dtheta
+    '''
     s = np.sin(theta)
     c = np.cos(theta)
     num = r * s * ( theta * (1.0 - lam * r**2) + r**2 * (lam - 1.0) * s * c )
@@ -21,6 +58,32 @@ def _rhs_christie(theta, r, lam, tiny=1e-14):
 
 def integrate_r_theta_christie(lam, R0, theta_max=np.pi-1e-5, n_theta=800,
                                eps_start=1e-6, atol=1e-9, rtol=1e-9):
+    """
+    Integrate r(theta) for Eq. 11 from theta=eps_start..theta_max.
+    Parameters:
+    -----------
+    lam : float
+        lambda parameter
+    R0  : float
+        standoff distance
+    theta_max : float
+        Maximum integration angle
+    n_theta : integer
+        theta resolution
+    eps_start : float
+        minimum angle; avoids numerical errors at the apex
+    atol : float
+        Absolute tolerance for the solver
+    rtol : float
+        Relative tolerance
+    
+    Returns:
+    --------
+    theta_vals : 1D array
+        Array with the angle values
+    r_vals : 1D array
+        Array with the r(theta) = R(theta)/R0 dimensionless values
+    """
     theta_eval = np.linspace(eps_start, theta_max, n_theta)
     r0 = 1.0
     def rhs(t, y):
@@ -34,24 +97,3 @@ def integrate_r_theta_christie(lam, R0, theta_max=np.pi-1e-5, n_theta=800,
     r_vals = sol.y[0]
     r_vals = np.where(np.isfinite(r_vals) & (r_vals > 0), r_vals, np.nan)
     return theta_vals, r_vals
-
-def make_surface_from_r_theta(r_vals, theta_vals, R0, lam, n_phi=200):
-    phi = np.linspace(0, 2*np.pi, n_phi)
-    TH, PH = np.meshgrid(theta_vals, phi)
-    interp = interp1d(theta_vals, r_vals, bounds_error=False, fill_value=np.nan)
-    r_grid = interp(TH)
-    alph = lam/(1-lam)
-    R_grid = r_grid * R0 * np.sqrt(1/(1+alph))
-    X = (R_grid * np.sin(TH) * np.cos(PH)) / AU
-    Y = (R_grid * np.sin(TH) * np.sin(PH)) / AU
-    Z = (R_grid * np.cos(TH)) / AU
-    return X, Y, Z
-
-def bow_shock_surface_christie(R0, lam=0.0, theta_max=np.pi-1e-5, n_theta=800, n_phi=200,
-                               eps_start=1e-6):
-    theta_vals, r_vals = integrate_r_theta_christie(lam, R0,
-                                                    theta_max=theta_max,
-                                                    n_theta=n_theta,
-                                                    eps_start=eps_start)
-    X, Y, Z = make_surface_from_r_theta(r_vals, theta_vals, R0, lam, n_phi=n_phi)
-    return X, Y, Z
